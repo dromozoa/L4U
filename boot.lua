50dac404817a2149418dd4f803eca7536122c989
Tomoyuki Fujimori <moyu@dromozoa.com>
2025-05-03 12:17:32 +0900

    レキサを分離。

--- boot.lua
+++ boot.lua
@@ -4,68 +4,127 @@ local major, minor = assert(_VERSION:match "^Lua (%d+)%.(%d+)$")
 local version = major * 10 + minor
 assert(version >= 54)
 
-local class = {}
-local metatable = { __index = class }
-local _1
-local _2
-local _3
-local _4
-
-function class.new(source)
-  return setmetatable({
-    source = source;
-    line = 1;
-    position = 1;
-  }, metatable)
-end
-
-function class:match(pattern)
-  local i, j, a, b, c, d = self.source:find("^"..pattern, self.position)
-  if i then
-    assert(self.position == i)
-    self.line = self.line + select(2, self.source:sub(self.position, j):gsub("\n", {}))
-    self.position = j + 1
-    _1 = a
-    _2 = b
-    _3 = c
-    _4 = d
-    return true
-  else
-    return false
-  end
-end
-
 -- 効率や速度は考えない
 -- 改行コードはLF限定（行番号を数える際）
-function class:parse()
-  while self.position <= #self.source do
-    -- 空白文字を無視する
-    if self:match "[\f\n\r\t\v ]+" then
+local function lexer(source)
+  local position = 1
+  local line = 1
 
-    -- コメントを無視する
-    elseif self:match "%-%-[^\n\r]*" then
+  local function _(pattern, name, conv)
+    local i, j, v = source:find("^"..pattern, position)
+    if i then
+      assert(i == position)
+      local s = source:sub(i, j)
+      local v = v or s
 
-    elseif self:match "function" then
-      print(self.line, self.position, "function")
+      local token = {
+        i = i;
+        j = j;
+        line = line;
+        name = name == nil and s or name;
+        source = s;
+        value = conv == nil and v or conv(v);
+      }
 
-    elseif self:match "end" then
-      print(self.line, self.position, "end")
+      position = j + 1
+      line = line + select(2, s:gsub("\n", {}))
 
-    elseif self:match "%(" then
-      print(self.line, self.position, "(")
-
-    elseif self:match "%)" then
-      print(self.line, self.position, ")")
-
-    elseif self:match "([A-Za-z_][0-9A-Za-z_]*)" then
-      print(self.line, self.position, "identifier", _1)
-
-    else
-      error("lexer error at line "..line)
+      return token
     end
   end
+
+  local tokens = {}
+  while position <= #source do
+    local token
+      =  _("%s+", false)
+      or _("%-%-[^\n\r]*", false)
+      or _("0[xX]%x+", "integer", tonumber)
+      or _("%d+", "integer", tonumber)
+
+      or _"and"
+      or _"break"
+      or _"do"
+      or _"else"
+      or _"elseif"
+      or _"end"
+      or _"false"
+      or _"for"
+      or _"function"
+      or _"goto"
+      or _"if"
+      or _"in"
+      or _"local"
+      or _"nil"
+      or _"not"
+      or _"or"
+      or _"repeat"
+      or _"return"
+      or _"then"
+      or _"true"
+      or _"until"
+      or _"while"
+
+      or _"%+"
+      or _"%-"
+      or _"%*"
+      or _"/"
+      or _"%%"
+      or _"%^"
+      or _"#"
+      or _"&"
+      or _"~"
+      or _"|"
+      or _"<<"
+      or _">>"
+      or _"//"
+      or _"=="
+      or _"~="
+      or _"<="
+      or _">="
+      or _"<"
+      or _">"
+      or _"="
+      or _"%("
+      or _"%)"
+      or _"{"
+      or _"}"
+      or _"%["
+      or _"%]"
+      or _"::"
+      or _";"
+      or _":"
+      or _","
+      or _"%."
+      or _"%.%."
+      or _"%.%.%."
+
+      or _("[A-Za-z][0-9A-Za-z]*", "name")
+
+    if not token then
+      print(position)
+      error("lexer error at line "..line)
+    end
+
+    if token.name then
+      print(token.name, token.value, token.line)
+      table.insert(tokens, token)
+    end
+
+  end
+
+  return tokens
+end
+
+local class = {}
+local metatable = { __index = class }
+
+function class.new(source)
+  return setmetatable({}, metatable)
+end
+
+function class:parse()
 end
 
 local source = io.read "*a"
-class.new(source):parse()
+lexer(source)
 -- io.write(source)
